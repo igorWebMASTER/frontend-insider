f554a4fc604579ef8eb4fda583b94d1ac20f490c
igorWebMASTER <igor_1917@hotmail.com>
2023-01-23 10:51:05 -0300

    docs: add readme images of lighthouse

---
+++ README.md
@@ -0,0 +1,11 @@
+# Insider Shop
+
+## Métricas lighthouse
+
+Desktop:
+
+![lighthouse-desktop](/assets/lighthouse-desktop.jpg)
+
+Mobile:
+
+![lighthouse-mobile](/assets/lighthouse-mobile.jpg)
--- src/products.js
+++ src/products.js
@@ -2,6 +2,44 @@
     style: 'currency',
     currency: 'BRL',
   });
+
+  const mediaQuerieMin = window.matchMedia("(min-width: 600px)");
+const mediaQuerieMax = window.matchMedia("(max-width: 600px)");
+
+function getHeaderElement(){
+  const  cartElement = document.querySelector(".cart");   
+  const  productList = document.getElementById("product-list");   
+  const  backIcon = document.querySelector(".back-icon");   
+  cartElement.style.display = "block";
+  backIcon.style.display = "block";
+  productList.style.display = "none";
+}
+
+function backToProductList(){
+  const  cartElement = document.querySelector(".cart");   
+  const  productList = document.getElementById("product-list");   
+  const  backIcon = document.querySelector(".back-icon");   
+  cartElement.style.display = "none";
+  backIcon.style.display = "none";
+  productList.style.display = "grid";
+}
+
+function handleMediaQuerieMin(mediaQuerieMin) {
+  if (mediaQuerieMin.matches) {
+    cartElement.style.display = "flex";
+  } 
+  
+  cartElement.style.display = "none";
+}
+
+function handleMediaQuerieMax(mediaQuerieMax) {
+    if (mediaQuerieMax.matches) {
+     cartElement.style.display = "block !important";
+   }
+}
+
+mediaQuerieMin.addEventListener('change', handleMediaQuerieMin);
+mediaQuerieMax.addEventListener('change', handleMediaQuerieMax);
   
   const BASE_URL = 'https://us-central1-insider-integrations.cloudfunctions.net/cart-api-fullstack-test'
   const cartLocalData = JSON.parse(localStorage.getItem('@cartLocalData'));
